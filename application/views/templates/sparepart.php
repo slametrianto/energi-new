c9512d23ca1b30db31e79a406f0c1e6ba37d0a62
slametrianto <slametrianto25@gmail.com>
2020-01-07 13:50:31 +0700

    energi termal solusi

---
+++ application/views/templates/sparepart.php
@@ -0,0 +1,219 @@
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="assets/img/sparepartq.jpg" class="d-block w-100" height="500">
+            <div class="carousel-caption d-none d-md-block">
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="alert alert-secondary" role="alert">
+    product & solution > power critical > sparepart
+</div>
+
+<section>
+    <div class="container mt-3">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="card border-primary mb-3">
+                    <div class="row no-gutters">
+                        <div class="col-md-4">
+                            <img src="assets/img/new/sembilan.jpg" class="card-img">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title text-primary">Humidifier Tank</h5>
+                                <!-- <p class="card-text">Dimensions (W x D x H)(mm) 930 x 995 x 1975, Operatioal weight(kg) 660, net cooling (kw) (downflow) 48.9, air flow 12500, type of filter Dry media type (G4 rating)-standard, electrial characteristics 400v(-15% -+ 10%)3PH + N - 50Hz.</p>
+                                <p class="card-text"><small class="text-muted">Liebert PEX3 30-100Kw Economical Precision Solution in Compact footprint.</small></p>
+                                <a href="readmore" class="btn btn-primary">Read more</a> -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="col-md-6">
+                <div class="card border-primary mb-3">
+                    <div class="row no-gutters">
+                        <div class="col-md-4">
+                            <img src="assets/img/new/tiga.jpg" class="card-img">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title text-primary">Fan Ax EBM A63630-AN01-01 +GR</h5>
+                                <!-- <p class="card-text">Dimensions (W x D x H) (mm) 1830 x 995 x 1975, Operational weight (kg) 670, 740, Net Cooling Capacity (kW) (downflow) 71.3, Net Cooling Capacity (kw) (upflow) 21200, No of Compressor & fan 2&2, Type of filter dry media type (G4 rating)- standard.</p>
+                                <p class="card-text"><small class="text-muted">Liebert PEX3 30-100Kw Economical Precision Solution in Compact footprint.</small></p>
+                                <a href="readmore" class="btn btn-primary">Read more</a> -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section>
+    <div class="container mt-3">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="card border-primary mb-3">
+                    <div class="row no-gutters">
+                        <div class="col-md-4">
+                            <img src="assets/img/new/tujuh.jpg" class="card-img">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title text-primary">Compressor</h5>
+                                <!-- <p class="card-text">Dimensions (W x D x H)(mm) 930 x 995 x 1975, Operatioal weight(kg) 660, net cooling (kw) (downflow) 48.9, air flow 12500, type of filter Dry media type (G4 rating)-standard, electrial characteristics 400v(-15% -+ 10%)3PH + N - 50Hz.</p>
+                                <p class="card-text"><small class="text-muted">Liebert PEX3 30-100Kw Economical Precision Solution in Compact footprint.</small></p>
+                                <a href="readmore" class="btn btn-primary">Read more</a> -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="col-md-6">
+                <div class="card border-primary mb-3">
+                    <div class="row no-gutters">
+                        <div class="col-md-4">
+                            <img src="assets/img/new/dua.jpg" class="card-img">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title text-primary">Control Board PACC</h5>
+                                <!-- <p class="card-text">Dimensions (W x D x H) (mm) 1830 x 995 x 1975, Operational weight (kg) 670, 740, Net Cooling Capacity (kW) (downflow) 71.3, Net Cooling Capacity (kw) (upflow) 21200, No of Compressor & fan 2&2, Type of filter dry media type (G4 rating)- standard.</p>
+                                <p class="card-text"><small class="text-muted">Liebert PEX3 30-100Kw Economical Precision Solution in Compact footprint.</small></p>
+                                <a href="readmore" class="btn btn-primary">Read more</a> -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section>
+    <div class="container mt-3">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="card border-primary mb-3">
+                    <div class="row no-gutters">
+                        <div class="col-md-4">
+                            <img src="assets/img/new/satu.jpg" class="card-img">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title text-primary">ICOM Control Board</h5>
+                                <!-- <p class="card-text">Dimensions (W x D x H)(mm) 930 x 995 x 1975, Operatioal weight(kg) 660, net cooling (kw) (downflow) 48.9, air flow 12500, type of filter Dry media type (G4 rating)-standard, electrial characteristics 400v(-15% -+ 10%)3PH + N - 50Hz.</p>
+                                <p class="card-text"><small class="text-muted">Liebert PEX3 30-100Kw Economical Precision Solution in Compact footprint.</small></p>
+                                <a href="readmore" class="btn btn-primary">Read more</a> -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="col-md-6">
+                <div class="card border-primary mb-3">
+                    <div class="row no-gutters">
+                        <div class="col-md-4">
+                            <img src="assets/img/new/enam.jpg" class="card-img">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title text-primary">Expansion Valve for Pex</h5>
+                                <!-- <p class="card-text">Dimensions (W x D x H) (mm) 1830 x 995 x 1975, Operational weight (kg) 670, 740, Net Cooling Capacity (kW) (downflow) 71.3, Net Cooling Capacity (kw) (upflow) 21200, No of Compressor & fan 2&2, Type of filter dry media type (G4 rating)- standard.</p>
+                                <p class="card-text"><small class="text-muted">Liebert PEX3 30-100Kw Economical Precision Solution in Compact footprint.</small></p>
+                                <a href="readmore" class="btn btn-primary">Read more</a> -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section>
+    <div class="container mt-3">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="card border-primary mb-3">
+                    <div class="row no-gutters">
+                        <div class="col-md-4">
+                            <img src="assets/img/new/lima.jpg" class="card-img">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title text-primary">Pex EC Fan</h5>
+
+                                <!-- <p class="card-text">Dimensions (W x D x H)(mm) 930 x 995 x 1975, Operatioal weight(kg) 660, net cooling (kw) (downflow) 48.9, air flow 12500, type of filter Dry media type (G4 rating)-standard, electrial characteristics 400v(-15% -+ 10%)3PH + N - 50Hz.</p>
+                                <p class="card-text"><small class="text-muted">Liebert PEX3 30-100Kw Economical Precision Solution in Compact footprint.</small></p>
+                                <a href="readmore" class="btn btn-primary">Read more</a> -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="col-md-6">
+                <div class="card border-primary mb-3">
+                    <div class="row no-gutters">
+                        <div class="col-md-4">
+                            <img src="assets/img/new/empat.jpg" class="card-img">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title text-primary">Pex Fan Speed Controll</h5>
+
+                                <!-- <p class="card-text">Dimensions (W x D x H) (mm) 1830 x 995 x 1975, Operational weight (kg) 670, 740, Net Cooling Capacity (kW) (downflow) 71.3, Net Cooling Capacity (kw) (upflow) 21200, No of Compressor & fan 2&2, Type of filter dry media type (G4 rating)- standard.</p>
+                                <p class="card-text"><small class="text-muted">Liebert PEX3 30-100Kw Economical Precision Solution in Compact footprint.</small></p>
+                                <a href="readmore" class="btn btn-primary">Read more</a> -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+<section>
+    <div class="container mt-3">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="card border-primary mb-3">
+                    <div class="row no-gutters">
+                        <div class="col-md-4">
+                            <img src="assets/img/new/delapan.jpg" class="card-img">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title text-primary">Infrared Humidifier</h5>
+                                <!-- <p class="card-text">Dimensions (W x D x H)(mm) 930 x 995 x 1975, Operatioal weight(kg) 660, net cooling (kw) (downflow) 48.9, air flow 12500, type of filter Dry media type (G4 rating)-standard, electrial characteristics 400v(-15% -+ 10%)3PH + N - 50Hz.</p>
+                                <p class="card-text"><small class="text-muted">Liebert PEX3 30-100Kw Economical Precision Solution in Compact footprint.</small></p>
+                                <a href="readmore" class="btn btn-primary">Read more</a> -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+        </div>
+    </div>
+</section>
